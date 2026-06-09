925be8252478e39f0f76435751d4f899e571ee98
Luis Andres Chuquimia Mamani <luisfofitochuqimia@gmail.com>
2026-06-09 22:01:50 +0000

    feat: (Repository): Realizamos configuraciones - Posibles probremas en el pom.xml  con spring security

---
+++ web_control/src/main/java/com/luis_andres/spring/web_control/repository/repositoryUser.java
@@ -0,0 +1,9 @@
+package com.luis_andres.spring.web_control.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luis_andres.spring.web_control.model.UserInfo;
+
+public interface repositoryUser extends JpaRepository<UserInfo, Long> {
+    
+}
--- web_control/src/main/java/com/luis_andres/spring/web_control/repository/repositoryUserImp.java
+++ web_control/src/main/java/com/luis_andres/spring/web_control/repository/repositoryUserImp.java
@@ -3,7 +3,6 @@ package com.luis_andres.spring.web_control.repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RepositoryMascotaImp  {
-
+public class repositoryUserImp {
     
 }
